a8cf1a2282912cc1ee9ab96f179f9599ba6600be
carma <cmarti@campuscamarafp.com>
2021-11-02 00:42:11 +0100

    ejercicios

--- clase/clase.php
+++ clase/clase.php
@@ -1,1 +1,40 @@
-<?php
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="clase.css">
+    <title>Document</title>
+</head>
+<body>
+<table>
+    <tr>
+        <td></td>
+        <td><img src="Laura2.png"></td>
+        <td></td>
+    </tr>
+    <tr>
+        <td><img src="Lucas2.png"></td>
+        <td></td>
+        <td><img src="Lucia2.png"></td>
+    </tr>
+    <tr>
+        <td><img src="Antonio2.png"></td>
+        <td></td>
+        <td><img src="Salva2.png"></td>
+    </tr>
+    <tr>
+        <td><img src="Sergio2.png"></td>
+        <td><img src="nico2.png"></td>
+        <td><img src="Angel2.png"></td>
+    </tr>
+    <tr>
+        <td><img src="Javi2.png"></td>
+        <td></td>
+        <td><img src="Carmen2.png"></td>
+    </tr>
+</table>
+</body>
+</html>
